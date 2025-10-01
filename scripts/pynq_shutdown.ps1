c1ff591ecc5979b8ac146c8c0a55b6e55e235521
Ryoga-exe <git@ryoga.dev>
2025-10-01 17:36:58 +0900

    feat: add pynq_shutdown script

---
+++ scripts/pynq_shutdown.ps1
@@ -0,0 +1,8 @@
+$port  = [System.IO.Ports.SerialPort]::new('COM3',115200,'None',8,'One');
+$port.Open()
+Start-Sleep -Milliseconds 500           # wait prompt
+$port.WriteLine('sudo shutdown -h now')
+Start-Sleep -Milliseconds 300           # wait prompt
+$port.WriteLine('xxxx')
+Start-Sleep 1
+$port.Close()
